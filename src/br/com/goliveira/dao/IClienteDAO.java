2f36a077d73136daec2b562331bc3be094787ece
unknown <Gabriel>
2025-08-11 17:22:55 -0300

    primeiro comit

---
+++ src/br/com/goliveira/dao/IClienteDAO.java
@@ -0,0 +1,18 @@
+package br.com.goliveira.dao;
+
+import br.com.goliveira.domain.Cliente;
+
+import java.util.Collection;
+
+public interface IClienteDAO {
+
+    public Boolean cadastrar(Cliente cliente);
+
+    public void excluir(Long cpf);
+
+    public void alterar(Cliente cliente);
+
+    public Cliente consultar(Long cpf);
+
+    public Collection<Cliente> buscarTodos();
+}
